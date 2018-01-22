ea8193e626f7ca85b7ee843586e046269a5293f0
Youri <Youri.sanders@hotmail.com>
2018-01-22 13:52:16 +0100

    test

--- src/logic/AdHocCar.java
+++ src/logic/AdHocCar.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.awt.*;
 
 public class AdHocCar extends Car {
-	private static final Color COLOR=Color.red;
+	private static final Color COLOR=Color.white;
 	
     public AdHocCar() {
     	Random random = new Random();
