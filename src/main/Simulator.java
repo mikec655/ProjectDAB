29c9dae4d4fe6d917bc0852aa4fce9c5a9231078
Mike Clarke <mikec655@gmail.com>
2018-01-22 13:34:26 +0100

    Alle views zijn nu te zien

--- src/main/Simulator.java
+++ src/main/Simulator.java
@@ -1,10 +1,12 @@
 package main;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import controller.Controller;
 import logic.Model;
@@ -21,8 +23,9 @@ public class Simulator extends JFrame implements ComponentListener{
 	private AbstractView carParkView;
 	private AbstractView tijdView;
 	private Controller controller;
-	private PercentView percentView;
-	private PieChartView pieChartView;
+	private AbstractView percentView;
+	private AbstractView pieChartView;
+	private JPanel viewPanel;
 	
 	
 	//super zorgt ervoor dat het een titel krijgt, die wordt boven in het frame weergegeven.
@@ -30,6 +33,7 @@ public class Simulator extends JFrame implements ComponentListener{
 		super("Pakeer Garage Simulator");
 		model = new Model();
 		controller = new Controller(model);
+		viewPanel = new JPanel();
 		carParkView = new CarParkView(model);
 		tijdView = new TimeView(model);
 		percentView = new PercentView(model);
@@ -40,6 +44,7 @@ public class Simulator extends JFrame implements ComponentListener{
 	//setup frame geeft een grootte aan het frame (hier komt alles in wat ingesteld moet worden binnen het frame)
 	private void setUpFrame() {
 		setLayout(new BorderLayout());
+		viewPanel.setLayout(new GridLayout(0, 1));
 		addComponents();
 		addComponentListener(this);
 		setSize(1280, 640);
@@ -50,11 +55,12 @@ public class Simulator extends JFrame implements ComponentListener{
 	
 	//addcomponents daarmee kun je views en controllers toevoegen aan het frame.
 	public void addComponents() {
+		getContentPane().add(viewPanel, BorderLayout.EAST);
 		getContentPane().add(carParkView, BorderLayout.CENTER);
 		getContentPane().add(controller, BorderLayout.SOUTH);
 		getContentPane().add(tijdView, BorderLayout.NORTH);
-		getContentPane().add(percentView, BorderLayout.EAST);
-		getContentPane().add(pieChartView, BorderLayout.EAST);
+		viewPanel.add(percentView, BorderLayout.EAST);
+		viewPanel.add(pieChartView, BorderLayout.EAST);
 	}
 
 	@Override
