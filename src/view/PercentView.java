c6ecfb18ad49e37fcb1987b60eed0354fca1d803
Youri <Youri.sanders@hotmail.com>
2018-01-23 15:55:22 +0100

    Profits aangepast en alles op 2 decimalen gezet (door Mike en Bernt)

--- src/view/PercentView.java
+++ src/view/PercentView.java
@@ -6,10 +6,13 @@ import logic.AdHocCar;
 import logic.Model;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
  
 public class PercentView extends AbstractView{
     private static final long serialVersionUID = 1337;
     private JLabel show;
+    private NumberFormat formatter = new DecimalFormat("#0.00");
    
     public PercentView(Model model) {
         super(model);
@@ -25,9 +28,17 @@ public class PercentView extends AbstractView{
     }
    
     public void updateView() {
+       
+       
         double placepercent=0;
+       
+       
+        String text = "";
         placepercent = getNumberOfPlaces(placepercent);
-        show.setText("<html>Het percentage open plaatsen is: " + String.valueOf(placepercent) + "% <br /> Profit: " + model.getprofitAv() + "</html>");
+        text += "<html>Het percentage open plaatsen is: " ;
+        text += formatter.format(placepercent) + "% <br /> ProfitAvg: ";
+        text += formatter.format(model.getProfitAv()) + "<br />"+ "Profit: " + formatter.format(model.getProfit())+"</html>";
+        show.setText(text);
        
     }
    
@@ -35,8 +46,6 @@ public class PercentView extends AbstractView{
  
                 double tempspots = 1.0 * (model.getNumberOfPlaces() * model.getNumberOfFloors() * model.getNumberOfRows());
                 placepercent = 1.0 * model.getNumberOfOpenSpots() / tempspots * 100.00;
-                placepercent = Math.round(placepercent * 10.00);
-                placepercent = placepercent / 10.00;
                 return placepercent;
                
         }
