ba77a06455b60d8eb7644192a9ead011a36f2d29
Youri <Youri.sanders@hotmail.com>
2018-01-24 09:29:45 +0100

    Berekening aangepast autoweergave

--- src/view/NumberView.java
+++ src/view/NumberView.java
@@ -24,11 +24,13 @@ public class NumberView extends AbstractView{
     public void updateView() {
         String text = "";
         int total = model.getNumberOfFloors() * model.getNumberOfRows() * model.getNumberOfPlaces();
+        int totalautos = model.getAmountOfResCars() + model.getAmountOfPassCars() + model.getAmountOfAdHocCars();
         text += "<html> ";
-        text += "Totaal aantal auto's: " + total + "<br>";
+        text += "Totaal aantal plaatsen: " + total + "<br>";
         text += "Aantal normale auto's: " + model.getAmountOfAdHocCars() + "<br>";
         text += "Aantal abbonnementhouder auto's: " + model.getAmountOfPassCars() + "<br>";
         text += "Aantal gereserveerde auto's: " + model.getAmountOfResCars() + "<br>";
+        text += "Totaal aantal auto's: " + totalautos + "<br>";
         text += "Aantal lege plekken: " + model.getNumberOfOpenSpots() + "</html>";
         show.setText(text);
        
